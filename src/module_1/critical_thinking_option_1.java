d7e6059df667cd9e244312baf240be81eaafb02b
madom <madom@192.168.0.7>
2025-06-17 15:43:49 -0600

    Initial commit

---
+++ src/module_1/critical_thinking_option_1.java
@@ -0,0 +1,24 @@
+package module_1;
+
+public class critical_thinking_option_1 {
+
+	public static void main(String[] args) {
+		
+		//set variables
+	    String firstName = "Shelly";
+        String lastName = "Rivers";
+        String streetAddress = "123 Seashore Way";
+        String city = "Oceanside";
+        String zipCode = "92127";
+
+        //print info
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+
+	}
+
+}
+
